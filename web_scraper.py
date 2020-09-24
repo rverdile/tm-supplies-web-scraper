9d682a8168d545fd2ceb202c41b714c62a1aa2e3
rverdile <ryanverdile@gmail.com>
2020-09-23 21:55:02 -0400

    file i/o changed to google sheets spreadsheet

--- web_scraper.py
+++ web_scraper.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import time
 import items
 import file_io as fio
+import gspread_pandas
 
 
 def get_distributer(url):
 
     return url.split('.')[1]
+
+# Config for google service account credentials
+c = gspread_pandas.conf.get_config('google_api_config','google_secret.json')
 
 # --- Structures for supported distributors --- #
 
@@ -26,9 +29,9 @@ digikey_items = {
 
 # --- Load in links --- #
 
-filename = 'items.xlsx'
+spreadsheet = gspread_pandas.Spread('GLV BOM',config=c)
 
-urls = fio.read_urls(filename)
+urls = fio.read_urls(spreadsheet)
 
 # --- Get distributors --- #
 
@@ -58,5 +61,5 @@ for url in urls:
 
 data.append(pd.DataFrame(digikey_items))
 
-fio.write_data(filename, data)
+fio.write_data(data, spreadsheet)
 
