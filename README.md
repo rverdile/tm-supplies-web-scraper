2dc88b22fe2ac065a067778d831abc025773be13
rverdile <ryanverdile@gmail.com>
2020-09-23 21:55:02 -0400

    file i/o changed to google sheets spreadsheet

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tm-supplies-web-scraper
-Updates Excel spreadsheet with item information like item name, stock, and price.
+Updates Google Sheets spreadsheet with item information like item name, stock, and price.
--- file_io.py
+++ file_io.py
@@ -1,27 +1,37 @@
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import time
 import items
+import gspread_pandas
 
-def read_urls(filename):
+def read_urls(spread,sheet_name=None):
+	"""
+	Reads urls from a particular sheet under the column "Link"
 
-    file = pd.read_excel(filename)
+	spread: a gspread object representing the entire Google Sheets spreadsheet (whereas 'sheet' is a tab on the spreadsheet)
+	sheet_name: name of sheet being read
 
-    urls = file['Link'].values
+	returns list of urls
+	"""
 
-    return urls
+	sheet = spread.sheet_to_df(sheet=sheet_name,index=False)
 
-def write_data(filename, data):
+	urls = sheet['Link'].values
 
-    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+	return urls
 
-    for d in data:
+def write_data(df, spread):
+	"""
+	Writes data contained in 'df' to 'spread' spreadsheet
 
-        item = items.Item(d['Link'][0])
+	df: pandas dataframe containing data
+	spread: a gspread object representing the entire Google Sheets spreadsheet (whereas 'sheet' is a tab on the spreadsheet)
+	"""
 
-        dstbtr = item.get_distributor()
 
-        d.to_excel(writer, sheet_name = dstbtr)
+	for d in df:
 
-    writer.save()
+		item = items.Item(d['Link'][0])
+
+		dstbtr = item.get_distributor()
+
+		spread.df_to_sheet(d,sheet=dstbtr,replace=True)
